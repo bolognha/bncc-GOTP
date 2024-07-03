b7ccafd8553879696f6834c664e1b2499f9ad74b
Thiago Henrique <ogaihto2728@gmail.com>
2024-07-02 22:17:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -77,4 +77,4 @@ function mousePressed() {
 
 let str =
   ['Valorização', 'Diversidade', 'Autonomia', 'Significativa', 'Habilidades', 'Consciente', 'Desenvolvimento', 'Prática', 'Resiliência', 'Intercultural', 'Saberes', 
-    'Valores', 'Flexível', 'Inclusão', 'Cidadania', 'Democrática', 'Protagonismo', 'Criatividade', 'Imaginação', 'Reflexão']
+    'Valores', 'Flexível', 'Inclusão', 'Cidadania', 'Democrática', 'Protagonismo', 'Criatividade', 'Imaginação', 'Reflexão', 'Catarse', 'Estesia', 'Contexto']
